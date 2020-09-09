1a9c3afcd47f8984ec35d5a0b5ca4eac95719cd5
Patrick C. Silva <patricksilva27@gmail.com>
2020-09-09 13:08:44 -0500

    Create lab08.py

---
+++ Day8/Lab/lab08.py
@@ -0,0 +1,15 @@
+## Exercise 1
+## Write a function using recursion to calculate the greatest common divisor of two numbers
+
+## Helpful link:
+## https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
+def gcd(x, y):
+
+
+## Problem 2
+## Write a function using recursion that returns prime numbers less than 121
+def find_primes(me = 121, primes = []):
+ 
+## Problem 3
+## Write a function that gives a solution to Tower of Hanoi game
+## https://www.mathsisfun.com/games/towerofhanoi.html
